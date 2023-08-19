720a82d4927451ca9132a3cc6c5959640a4e3459
thinkingmore <peterkevin823@gmail.comm>
2023-08-19 09:39:54 +0600

    added project description and hero section

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -7,8 +7,6 @@ exports.homepage = async (req, res) => {
       title: "Add Note App",
       description: "Take notes.",
     }
-
-    console.log(req.user,"we got the user")  
     res.render('pages/index', {
       user: req.user,
       layout: "../views/layouts/layout",
